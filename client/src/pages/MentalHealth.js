13a77e1638164100bec19066563599c8e4fad5af
Manny <manroven-singh@hotmail.com>
2022-08-20 11:59:33 +0930

    adding mental health section

---
+++ client/src/pages/MentalHealth.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MentalHealth.css";
+
+const MentalHealth = () => {
+  return (
+    <span className="font-link">
+      <main>
+        <div className="getin-background">
+          <div className="getin-para flex-row justify-center">
+            <div className="getin-heading flex-row justify-center">
+              Benefits of exercise for mental health
+            </div>
+            <br></br>
+            <br></br>- it can reduce your stress and symptoms of mental health conditions like depression and anxiety
+            <br></br>
+            <br></br>- Coz Exercise releases chemicals like endorphins and serotonin that improve your mood.
+            <br></br>
+          </div>
+        </div>
+      </main>
+    </span>
+  );
+};
+
+export default MentalHealth;
