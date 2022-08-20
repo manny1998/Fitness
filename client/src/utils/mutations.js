bb6fae187e3323e13b71f4e4ae88ea80b8498192
Manny <manroven-singh@hotmail.com>
2022-08-20 11:46:55 +0930

    adding mutations & queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,25 +1,50 @@
-import { gql } from '@apollo/client';
-
-export const ADD_USER = gql`
-mutation AddUser($username: String!, $email: String!, $password: String!) {
-  addUser(username: $username, email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
-      email
-    }
-  }
-}`
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-mutation Login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
-      email
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
-}`
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_WORKOUTPLAN = gql`
+  mutation addWorkoutplan($name: String!, $workoutplanText: String!, $user_id: ID!) {
+    addWorkoutplan(name: $name, workoutplanText: $workoutplanText, user_id: $user_id) {
+      token
+      workoutplan {
+        name
+        workoutplanText
+        user_id
+      }
+    }
+  }
+`;
+
+export const ADD_PB = gql`
+  mutation addPb($pbText: String!, $user_id: ID!) {
+    addPb(pbText: $pbText, user_id: $user_id) {
+      token
+      pb {
+        pbText
+        user_id
+      }
+    }
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,63 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_ME= gql`
-query Me {
-  me {
-    _id
-    username
-    email
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+      }
+    }
   }
-}
+`;
+
+export const QUERY_ALL_WORKOUTPLANS = gql`
+  {
+    workoutplans {
+      name
+      workoutplanText
+      createdAt
+      user_id
+    }
+  }
+`;
+
+export const QUERY_SINGLE_WORKOUTPLAN= gql`
+  query workoutplan($id: ID!) {
+    workoutplan(_id: $id) {
+      workoutplan_id
+      name
+      workoutplanText
+      createdAt
+      user_id
+    }
+  }
+`;
+
+export const QUERY_ALL_PBS = gql`
+  {
+    pbs {
+      name
+      pbText
+      completed
+      createdAt
+      user_id
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PB = gql`
+  query pb($id: ID!) {
+    pb(_id: $id) {
+      pb_id
+      pbText
+      completed
+      createdAt
+      user_id
+    }
+  }
 `;
