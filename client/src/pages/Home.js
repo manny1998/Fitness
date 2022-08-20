447db988ba84a22a351eba9d3421e5dda8432ce2
Manny <manroven-singh@hotmail.com>
2022-08-20 11:49:48 +0930

    adding home page and design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import "./Home.css";
 const Home = () => {
-
   return (
-    <main>
-      <div className="flex-row justify-center">
-        homepage
-      </div>
-    </main>
+    <span className="font-link">
+      <main>
+        <div className="home-background">
+          <div className="home-para flex-row justifty-center">
+            <br></br>
+            This is the best tool<br></br>
+            to help you achieve your fintess goals <br></br>
+            <br></br>
+            <br></br>
+            So let's get started...
+            <br></br>
+            <br></br>
+          </div>
+        </div>
+      </main>
+    </span>
   );
 };
 
