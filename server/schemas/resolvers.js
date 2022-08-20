b8188941370728b5f820a1d82e664227117a83f3
Manny <manroven-singh@hotmail.com>
2022-08-20 11:43:24 +0930

    adding typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,22 +1,36 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Pb, Workoutplan } = require("../models");
+const { signToken } = require("../utils/auth");
+
+console.log("Start Resolvers");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find();
+      return User.find().populate("workoutplans");
     },
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ email: context.user.email});
-      }
-      throw new AuthenticationError('You must be signed in');
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate("workoutplans");
+    },
+    workoutplans: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Workoutplan.find(params).sort({ createdAt: -1 });
+    },
+    workoutplan: async (parent, { workoutplan_id }) => {
+      return Workoutplan.findOne({ user_id: workoutplan_id });
+    },
+    pbs: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Pb.find(params).sort({ createdAt: -1 });
+    },
+    pb: async (parent, { pb_id }) => {
+      return Pb.findOne({ user_id: pb_id });
     },
   },
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
+      // First we create the user
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
@@ -25,20 +39,50 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError("No user found with this email address");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
-
+      // Return an `Auth` object that consists of the signed token and user's information
       return { token, user };
+    },
+
+    addWorkoutplan: async (parent, { workoutplanText, user_id }) => {
+      const workoutplan = await Workoutplan.create({ workoutplanText, user_id });
+
+      await User.findOneAndUpdate(
+        { username: user_id },
+        { $addToSet: { workoutplans: workoutplan.workoutplan_id } }
+      );
+
+      return workoutplan;
+    },
+
+    addPb: async (parent, { pbText, user_id }) => {
+      const pb = await Pb.create({ pbText, user_id });
+
+      await User.findOneAndUpdate(
+        { username: user_id },
+        { $addToSet: { pbs: pb.pb_id } }
+      );
+
+      return pb;
+    },
+    removeWorkoutplan: async (parent, { workoutplan_id }) => {
+      return Workoutplan.findOneAndDelete({ user_id: workoutplan_id });
+    },
+    removePb: async (parent, { pb_id }) => {
+      return Pb.findOneAndDelete({ user_id: pb_id });
     },
   },
 };
+
+console.log("End Resolvers");
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,14 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
+
+console.log("start typeDefs");
 
 const typeDefs = gql`
   type User {
     _id: ID
     username: String
     email: String
+    Workoutplan: String
+    Pb: String
   }
 
   type Auth {
@@ -12,15 +16,40 @@ const typeDefs = gql`
     user: User
   }
 
+  type Workoutplan {
+    workoutplan_id: ID
+    name: String
+    workoutplanText: String
+    createdAt: String
+    user_id: ID
+  }
+
+  type Pb {
+    pb_id: ID
+    pbText: String
+    completed: Boolean
+    createdAt: String
+    user_id: ID
+  }
+
   type Query {
     users: [User]
-    me: User
+    user(username: String!): User
+    workoutplans(username: String): [Workoutplan]
+    workoutplan(workoutplan_id: ID!): Workoutplan
+    pbs(username: String!): [Pb]
+    pb(pb_id: ID!): Pb
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    addWorkoutplan(name: String!, workoutplanText: String!, user_id: ID!): Workoutplan
+    addPb(pb: String!, user_id: ID!): Pb
+    removeWorkoutplan(workoutplan_id: ID!): Workoutplan
+    removePb(pb_id: ID!): Pb
   }
 `;
+console.log("end typeDefs");
 
 module.exports = typeDefs;
