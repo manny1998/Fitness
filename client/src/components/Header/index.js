9060c9120e8376c7180174515f124ae0f7a7a911
Manny <manroven-singh@hotmail.com>
2022-08-20 11:55:48 +0930

    finishing up header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,49 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-
-import Auth from '../../utils/auth';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import Auth from "../../utils/auth";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
   return (
-    <Nav>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/">
-          Home
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={Link} to="/profile">
-          Profile
-        </Nav.Link>
-      </Nav.Item>
-      {Auth.loggedIn() ? (
-        <>
-          <Nav.Item>
-            <Nav.Link onClick={logout}>
-              Logout
-            </Nav.Link>
-          </Nav.Item>
-        </>
-      ) : (
-        <>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/signup" >
-              Signup
-            </Nav.Link>
-          </Nav.Item>
-        </>
-      )}
-    </Nav>
+    <header className="nav-container text-light mb-4 py-3 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <span className="font-link">
+          <div>
+            {Auth.loggedIn() ? (
+              <>
+                <span>Hello,  {Auth.getProfile().data.username}!</span>
+                <Link className="btn btn-lg btn-light m-2" to="/fitnesshighscores">
+                  My Fitness highscores
+                </Link>
+                <Link className="btn btn-lg btn-light m-2" to="/workoutplans">
+                  My workoutplans
+                </Link>
+                <Link
+                  to="/"
+                  className="btn btn-lg btn-light m-2"
+                  onClick={logout}
+                >
+                  Logout
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link className="btn btn-lg btn-light m-2" to="/mentalhealth">
+                  Mental Health Benefits
+                </Link>
+                <Link
+                  className="btn btn-lg btn-light m-2"
+                  to="/physicalbenefits"
+                >
+                  Physical Health Benefits
+                </Link>
+                <Link className="btn btn-lg btn-light m-2" to="/login">
+                  Login
+                </Link>
+
+                <Link className="btn btn-lg btn-light m-2" to="/signup">
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </span>
+      </div>
+    </header>
   );
 };
 
