2d45094be92c09e54e447244f7b1994a96455cbd
Manny <manroven-singh@hotmail.com>
2022-08-20 12:29:17 +0930

    making amendments due to errors when trying to login

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '../.env' )});
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/final-project-test', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
